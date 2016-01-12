42548e26b3c113ca3c6de2eba403d3efb4de4396
Marek Tihkan <marektihkan@gmail.com>
2016-01-12 11:43:50 +0200

    Prints out the results of test script

--- test.rb
+++ test.rb
@@ -4,4 +4,4 @@ require 'teamweek/api/test/http_client'
 token = ""
 
 http_client = Teamweek::Api::Test::HttpClient.new('Authorization' => "Bearer #{ token }")
-Teamweek::Api::Client.new(http_client, 181896).fetch_tasks
+puts Teamweek::Api::Client.new(http_client, 181896).fetch_tasks
